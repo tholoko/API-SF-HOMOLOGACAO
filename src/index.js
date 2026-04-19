23dd7047cac0339bee7355d7ece5fe2d534f152c
Lzaro Kallebe dos Santos <lazaro.santos@sociedadefranciosi.com.br>
2026-04-19 15:34:19 -0300

    dfad

--- src/index.js
+++ src/index.js
@@ -8955,6 +8955,8 @@ app.post('/api/reservas-carro-formulario/:id/aprovar', async (req, res) => {
       });
     }
 
+    console.log(reserva.status_solicitacao);
+
     if (normalizarStatusReserva(reserva.status_solicitacao) !== 'PENDENTE FROTA') {
       await conn.rollback();
       return res.status(400).json({
